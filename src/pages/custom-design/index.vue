adb7135877641eef79152b3da35b9958a5fb0ff9
sfdd <sfdd@sfdddeiMac.local>
2017-05-19 12:39:39 +0800

    minified imgs

--- src/pages/custom-design/index.vue
+++ src/pages/custom-design/index.vue
@@ -2,29 +2,36 @@
   <market>
     <market-item>
       <div class="what">
-        <h1 class="Intro__title">一对一定制是什么</h1>
-        <p class="Intro__desc">
-          一个设计团队，服务一家客户，
-          <br> 规划今后的生活，您仅需面对一个主案设计师，
-          <br> 剩下的交给我们。
-          <br> 只有充分的项目经验、优良的沟通与表达能力，
-          <br> 才能懂你与你的家人，
-          <br> 主案设计师须拥有十年以上的工作资历，
-          <br> 十个以上的大型项目经验，
-          <br> 并能掌握深化、家具、木作的协作分工。
-        </p>
-        <img class="what__img"
-             src="../../assets/custom-design/what.png"
-             width="60%"
-             alt="">
+        <section class="what__hd _tc"
+                 grid='middle'>
+          <div>
+            <h1 class="Intro__title animated fade">一对一定制是什么</h1>
+            <p class="Intro__desc  animated slideInUp fade">
+              一个设计团队，服务一家客户，
+              <br> 规划今后的生活，您仅需面对一个主案设计师，
+              <br> 剩下的交给我们。
+              <br> 只有充分的项目经验、优良的沟通与表达能力，
+              <br> 才能懂你与你的家人，
+              <br> 主案设计师须拥有十年以上的工作资历，
+              <br> 十个以上的大型项目经验，
+              <br> 并能掌握深化、家具、木作的协作分工。
+            </p>
+          </div>
+        </section>
+        <section class="what__bd _tc">
+          <img class="what__img  animated fade"
+               src="../../assets/custom-design/what.png"
+               alt="">
+        </section>
       </div>
     </market-item>
 
     <market-item>
       <div class="who">
-        <h1 class="Intro__title">哪些客户需要一对一的定制设计？
+        <h1 class="Intro__title  animated fade">哪些客户需要一对一的
+          <br>定制设计？
         </h1>
-        <p class="Intro__desc">
+        <p class="Intro__desc  animated fade slideInUp">
           我们认为整装预案能满足
           <br> 大部分客户的需求，
           <br> 但是当有如下情况时，需考虑一对一定制设计，
@@ -35,7 +42,7 @@
             <br> 居住者多或决策人多
           </span>
         </p>
-        <img class="who__img"
+        <img class="who__img  animated fade"
              src="../../assets/custom-design/who.jpg"
              width="100%"
              alt="">
@@ -44,17 +51,23 @@
 
     <market-item>
       <div class="flow">
-        <h1 class="Intro__title">
-          定制流程
-        </h1>
-        <p class="Intro__desc">
-          优秀的作品来自设计师与您与全部家庭成员的充分协作，
-          <br> 严谨的流程与高效的沟通是基础。
-        </p>
-        <img class="flow__img"
-             src="../../assets/custom-design/flow.png"
-             width="60%"
-             alt="">
+        <section class="flow__hd _tc"
+                 grid='middle'>
+          <div>
+            <h1 class="Intro__title  animated fade">
+              定制流程
+            </h1>
+            <p class="Intro__desc  animated fade slideInUp">
+              优秀的作品来自设计师与您与全部家庭成员的充分协作，
+              <br> 严谨的流程与高效的沟通是基础。
+            </p>
+          </div>
+
+        </section>
+        <section class="flow__bd _tc">
+          <img class="flow__img  animated fade"
+               src="../../assets/custom-design/flow.png">
+        </section>
       </div>
     </market-item>
 
